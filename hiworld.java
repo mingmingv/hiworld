3c64987e869fb5fc9365bd8f9def8523c354d50b
mingmingv <qq1014170583@163.com>
2018-08-10 23:35:02 +0800

    test

---
+++ hiworld.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IDEA
+ * author:WuJiaMing
+ * Date:2018/8/10
+ * Time:23:27
+ */
+public class hiworld {
+    public static void main(String [] args){
+        System.out.println("Hello World!");
+    }
+}
